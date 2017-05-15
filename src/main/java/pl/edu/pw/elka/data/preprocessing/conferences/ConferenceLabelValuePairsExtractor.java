6991b89a22c9ea782d5c823caf360d9829e9140d
Paweł Koszelew <koszelew@gmail.com>
2017-05-15 22:55:30 +0200

    Implemented conferences proper label-value pairs extraction

--- src/main/java/pl/edu/pw/elka/data/preprocessing/conferences/ConferenceLabelValuePairsExtractor.java
+++ src/main/java/pl/edu/pw/elka/data/preprocessing/conferences/ConferenceLabelValuePairsExtractor.java
@@ -2,8 +2,10 @@ package pl.edu.pw.elka.data.preprocessing.conferences;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +13,7 @@ import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 
 import gate.util.GateException;
+import pl.edu.pw.elka.data.preprocessing.CleanTextExtractor;
 import pl.edu.pw.elka.data.preprocessing.FilesLoader;
 import pl.edu.pw.elka.data.preprocessing.LabelValuePair;
 import pl.edu.pw.elka.data.preprocessing.LabelValuePairsExtractor;
@@ -18,18 +21,44 @@ import pl.edu.pw.elka.ner.NamedEntityLabelMapper;
 import pl.edu.pw.elka.ner.NamedEntityRecognizer;
 
 public class ConferenceLabelValuePairsExtractor implements LabelValuePairsExtractor {
-
+	
     private static final String UTF_8 = "UTF-8";
     private static final List<String> ENTITY_NAMES = Arrays.asList("Person", "Location", "Date");
 	
 	@Override
     public List<LabelValuePair> extract(String data) {
-        return Arrays.stream(ConferenceLabel.values())
+		// Extract all label-value pairs
+        List<LabelValuePair> extraction = Arrays.stream(ConferenceLabel.values())
                 .flatMap(tag -> Jsoup.parse(data)
                         .select(tag.tagName())
                         .stream()
                         .map(e -> LabelValuePair.from(tag.toString(), e.text())))
                 .collect(Collectors.toList());
+        // Remove duplicates
+        List<LabelValuePair> helpList = new ArrayList<LabelValuePair>();
+        HashSet<String> lookup = new HashSet<String>();
+        for (LabelValuePair pair : extraction) {
+			if(lookup.add(pair.label + pair.value.toLowerCase()))
+				helpList.add(pair);
+		}
+        // Merge sibling rows with the same label
+        CleanTextExtractor cleanTextExtractor = new ConferenceCleanTextExtractor();
+        if(helpList.size() > 0) {
+        	String currentValue = helpList.get(0).value;
+        	extraction.clear();
+        	for (int i = 1; i < helpList.size(); i++) {
+        		if(helpList.get(i).label == helpList.get(i-1).label) {
+        			currentValue = currentValue + " " + helpList.get(i).value;
+        		}
+        		else {
+        			extraction.add(LabelValuePair.from(helpList.get(i-1).label, cleanTextExtractor.extract(currentValue)));
+        			currentValue = helpList.get(i).value;
+        		}
+        	}
+			extraction.add(LabelValuePair.from(helpList.get(helpList.size()-1).label,  cleanTextExtractor.extract(currentValue)));
+        }
+        
+        return extraction;
     }
     	
 	public static void main(String[] args) throws IOException, GateException {
@@ -45,7 +74,7 @@ public class ConferenceLabelValuePairsExtractor implements LabelValuePairsExtrac
         LabelValuePairsExtractor extractor = new ConferenceLabelValuePairsExtractor();
         
         // getting list of conferences labels
-        List<LabelValuePair> pairs = new FilesLoader().loadFiles(conferencesDir)
+        List<LabelValuePair> pairs = new FilesLoader().loadFiles(conferencesDir, new String[]{"html"})
                 .stream()
                 .map(f -> {
                     try {
@@ -56,6 +85,16 @@ public class ConferenceLabelValuePairsExtractor implements LabelValuePairsExtrac
                 })
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
+        
+        // removing dupplicates
+        List<LabelValuePair> helpList = new ArrayList<LabelValuePair>();
+        HashSet<String> lookup = new HashSet<String>();
+        for (LabelValuePair pair : pairs) {
+			if(lookup.add(pair.label + pair.value.toLowerCase()))
+				helpList.add(pair);
+		}
+        pairs = helpList;
+        
         
         // clean text file
         String cleanConferenceFileText = FileUtils.readFileToString(new File(cleanConferencesFile), UTF_8);
